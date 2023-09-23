0a7faea42fbf04ab4b1a8143e8f46800be625670
kiri <kirillknizhov@mail.ru>
2023-09-23 21:04:40 +0700

    survival api output fix

--- services/survival_service.R
+++ services/survival_service.R
@@ -6,8 +6,7 @@ rm(list = ls(all = TRUE))
 getwd()
 
 SurvivalService <- function(df, nocc, output) {
-  sink(paste(output, "res.txt", sep = "/"))
-
+  options(width=10000)
 
   final2 <- df
   final2$Fate <- as.factor(final2$Fate)
@@ -32,7 +31,6 @@ SurvivalService <- function(df, nocc, output) {
   rftest.getImportance(rfData)
 
   meanImportance <- rftest.getImportance(rfData)
-
   importantVariables <- row.names(meanImportance[meanImportance$MeanDecreaseGini >= 0.5, , drop = FALSE])
 
   res <- lapply(0:length(importantVariables), function(x) combn(length(importantVariables), x))
@@ -53,17 +51,17 @@ SurvivalService <- function(df, nocc, output) {
       table <- collect.models()
       tables[nrow(tables) + 1, colnames(table$model.table)] <- table$model.table
       res <- cbind(res, list(mark, table))
-      print(table$mark)
-      print(table$mark$results$real$estimate^nocc)
-    }
+   }
     return(list(res, tables))
   }
 
   nest.full_results <- markrun.nestall()
+
   nest.table <- nest.full_results[[2]][order(nest.full_results[[2]]$AICc), , drop = FALSE]
-  print(nest.table)
+
 
   sorted <- nest.full_results[[1]][, order(nest.full_results[[2]]$AICc), drop = FALSE]
+
 
   drawParamPlot <- function(param, netl_par_index = 1, sequence_, name_, result) {
     df <- data.frame(sequence_)
@@ -85,12 +83,18 @@ SurvivalService <- function(df, nocc, output) {
     for (param in covariates) {
       maxParamVal <- max(rfData[param])
       minParamVal <- min(rfData[param])
-      print(covariates)
       jpeg(paste(output, paste(paste(covariates, collapse = "+"), paste(param, "jpg", sep = "."), sep = "_"), sep = "/"))
       drawParamPlot(param = param, sequence_ = seq(minParamVal, maxParamVal, length = 100), name_ = param, result = sorted[1,][[index]])
       dev.off()
     }
   }
 
+  sink(paste(output, "res.txt", sep = "/"))
+  print("Mean variable importance according to Random forest:")
+  print(meanImportance)
+  print("Tested models:")
+  print(nest.table)
+  print("Best model result:")
+  print(sorted[1,][[1]]$results)
   sink()
 }
